d54d1c39f00aa7f92c51236b96eee9b7b9e91aa9
romunov <romunov@gmail.com>
2015-07-31 09:49:40 +0200

    MH algo now works

--- mcmcmc_examples.R
+++ mcmcmc_examples.R
@@ -1,16 +1,22 @@
 # a script with examples from the book (see README on my github page) using Metropolis-Hastings sampler
 # from page 443
 
-LRMH <- function(y, mu0, sigma0, theta, delta, niter) {
+LRMH <- function(formula, mu0, sigma0, theta, delta, niter, data) {
   out <- rep(NA, niter)
+  get.data <- all.vars(formula)
+  y <- data[, get.data[1]]
+  x <- data[, get.data[2]]
   
   for (i in 1:niter) {
+    # this part should be generalized to accomodate more than one x, probably
+    # something along the lines of %*% and conting the number of explanatory
+    # variables
     theta.cand <- rnorm(1, mean = theta, sd = delta)
     
-    loglik.cand <- sum(dbinom(y, size = 1, prob = exp(theta.cand)/(1 + exp(theta.cand)), log = TRUE))
+    loglik.cand <- sum(dbinom(y, size = 1, prob = exp(theta.cand * x)/(1 + exp(theta.cand * x)), log = TRUE))
     logprior.cand <- dnorm(theta.cand, mean = mu0, sd = sigma0, log = TRUE)
 
-    loglik <- sum(dbinom(y, size = 1, prob = exp(theta)/(1 + exp(theta)), log = TRUE))
+    loglik <- sum(dbinom(y, size = 1, prob = exp(theta * x)/(1 + exp(theta * x)), log = TRUE))
     logprior <- dnorm(theta, mean = mu0, sd = sigma0, log = TRUE)
     
     ratio <- exp((loglik.cand + logprior.cand) - (loglik + logprior))
@@ -39,8 +45,8 @@ sim.data <- data.frame(y = y, x = x)
 plot(sim.data)
 
 # analyze using a "classical" GLM
-summary(glm(y ~ -1 + x, data = sim.data, family = binomial)) # works
+summary(glm(y ~ -1 + x, data = sim.data, family = binomial))
 
 # analyze using a MH algo
-mh.res <- LRMH(y = sim.data$y, mu0 = 3, sigma0 = 2, theta = 3, delta = 3, niter = 5000)
-hist(mh.res, xlab = "theta") # wrong, I missed something
+mh.res <- LRMH(y ~ x, data = sim.data, mu0 = 3, sigma0 = 2, theta = 3, delta = 3, niter = 50000)
+hist(mh.res, xlab = "theta")
